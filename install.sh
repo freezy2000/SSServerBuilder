7a93da6443e8476764e2a6eff124951c6063b422
Zhang Yuan <45662983+freezy2000@users.noreply.github.com>
2019-01-26 14:06:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,17 @@ mv -f shadowsocks.json /etc/shadowsock/
 curl https://raw.githubusercontent.com/freezy2000/SSServerBuilder/master/ssstart.sh -o ssstart.sh
 curl https://raw.githubusercontent.com/freezy2000/SSServerBuilder/master/ssstart.sh -o ssstop.sh
 
-
 chmod +x ssstart.sh
 chmod +x ssstop.sh
+
+##Modify firewall
+firewall-cmd --zone=public --add-port=1357/tcp --permanent
+
+##Reload firewall
+firewall-cmd --reload
+
+##List all open ports
+firewall-cmd --zone=public --list-ports
 
 ##Modify congfig file
 read -n1 -p "Please modify shadowsocks.json"
